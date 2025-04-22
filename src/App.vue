6025877fe146e247925aad6a63b485ca29e2acad
chennaijia <naijia030@gmail.com>
2025-04-22 11:03:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- src/App.vue -->
+<template>
+  <div class="container">
+    <Sidebar />
+    <iframe
+      name="contentFrame"
+      :src="frameSrc"
+      style="width: 100%; height: 90vh; border: none"
+    ></iframe>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const frameSrc = ref('pages/homepage.html')
+</script>
+
+<style scoped>
+/* 可放主頁樣式 */
+</style>
