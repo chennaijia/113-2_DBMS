daa7e75dcfe5fa10db7ed0021918bf7477d0c050
MinYu <112306075@g.nccu.edu.tw>
2025-05-25 16:15:47 +0800

    FIX DELETE QB

--- src/api/qb.ts
+++ src/api/qb.ts
@@ -3,7 +3,7 @@ import { api } from './index';
 //export const fetchQBs  = ()                          => api.get   ('/books');
 export const createQB  = (data: any)                 => api.post  ('/books', data);
 export const updateQB  = (id: number, data: any)     => api.put   (`/books/${id}`, data);
-export const deleteQB  = (id: number)                => api.delete(`/books/${id}`);
+export const deleteQB = (id: number)                 => api.delete(`/books/${id}`);
 export const copyQB    = (id: number)                => api.post  (`/books/${id}/copy`);
 
 export const fetchQBs = () => {
